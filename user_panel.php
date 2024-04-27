9f6e0961207dcfb77c3236e26ad4bd50448b4063
salarsalimi <salarsalimi990@gmail.com>
2024-04-27 06:59:58 -0400

    Section01 ( Adding simple template and CSS )

---
+++ user_panel.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weblog Homepage</title>
+    <link rel="stylesheet" href="statics/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Our Weblog</h1>
+    </header>
+    <nav>
+        <a href="#">Panel</a>
+        <a href="#">Write</a>
+        <a href="#">Post</a>
+        <a href="#">Setting</a>
+    </nav>
+    <div class="container">
+        <div class="post">
+            <h2>Post Title 1</h2>
+            <p>This is the content of the first blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</p>
+        </div>
+        <div class="post">
+            <h2>Post Title 2</h2>
+            <p>This is the content of the second blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</p>
+        </div>
+        <div class="post">
+            <h2>Post Title 3</h2>
+            <p>This is the content of the third blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</p>
+        </div>
+    </div>
+</body>
+</html>
